d15a83e3998e83ca0e11a5a1fde8d5da72c15f36
pxnx <loool1912@web.de>
2020-12-05 23:34:47 +0100

    Entry

--- bot.py
+++ bot.py
@@ -65,7 +65,7 @@ def incoming_text(update: Update, context: CallbackContext):
         ))
 
 
-def button(update: Update, context: CallbackContext) -> None:
+def submit(update: Update, context: CallbackContext) -> None:
             query = update.callback_query
             position = int(query.data)
 
@@ -201,6 +201,8 @@ def main():
     dp.add_handler(MessageHandler(Filters.text, incoming_text))
 
     # dp.add_handler(MessageHandler(Filters.a, incoming_media))
+
+    dp.add_handler(CallbackQueryHandler(submit))
 
     dp.add_error_handler(error)  # REMOVE FOR STACKTRACE!!
 
