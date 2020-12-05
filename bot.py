b30c9c55e79c5774a066671681234b2cec718e24
pxnx <loool1912@web.de>
2020-12-06 00:48:18 +0100

    Entry

--- bot.py
+++ bot.py
@@ -188,7 +188,7 @@ def main():
 
     dp.add_handler(CallbackQueryHandler(submit))
 
-    dp.add_error_handler(error)  # REMOVE FOR STACKTRACE!!
+ #   dp.add_error_handler(error)  # REMOVE FOR STACKTRACE!!
 
     updater.start_webhook(listen="0.0.0.0", port=int(PORT), url_path=TOKEN)
     updater.bot.setWebhook('https://ptb-militaernews.herokuapp.com/' + TOKEN)
