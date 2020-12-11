1725a2c4660d576f0887ecdf00e338e348ef351f
pxnx <loool1912@web.de>
2020-12-11 13:52:30 +0100

    Entry

--- bot-conv.py
+++ bot-conv.py
@@ -30,7 +30,7 @@ def start(update: Update, context: CallbackContext) -> int:
         'Hi! My name is Professor Bot. I will hold a conversation with you. '
         'Send /cancel to stop talking to me.\n\n'
         'Are you a boy or a girl?',
-        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True),
+        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True,resize_keyboard=True),
     )
 
     return GENDER
