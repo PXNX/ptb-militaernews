69085ec8928b857c42d8929b6aeb1d60c4f79062
pxnx <loool1912@web.de>
2021-01-14 21:16:21 +0100

    change post filter

--- bot-conv.py
+++ bot-conv.py
@@ -265,7 +265,7 @@ if __name__ == '__main__':
     updater = Updater(TOKEN)
     dp = updater.dispatcher
 
-    dp.add_error_handler(error)
+  #  dp.add_error_handler(error)
 
     dp.add_handler(CommandHandler('start', start, filters=Filters.chat(chat_id=VERIFIED_USERS)))
 
