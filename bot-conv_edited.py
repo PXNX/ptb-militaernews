f7f98de92f2a66cfb1b4f1334eec0276251a7ed9
pxnx <loool1912@web.de>
2021-01-15 19:00:43 +0100

    change post filter

--- bot-conv_edited.py
+++ bot-conv_edited.py
@@ -249,7 +249,7 @@ def message_preview(update: Update, context: CallbackContext):
         debug(context.user_data)
         
         # how to copy it?
-        msg = context.bot.send_media_group(chat_id=update.message.chat_id, media=files)
+        msg = context.bot.send_media_group(chat_id=update.message.chat_id, media=files, reply_markup=SHOW_MORE)
         debug("[DEBUG] message_preview: var msg changed to: context.bot.send_media_group(chat_id=update.message.chat_id, media=files, reply_markup=SHOW_MORE)")
     
     context.user_data['post'] = msg
